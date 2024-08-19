44de4e46a9c288e1eb39b9fc542565872a0d3e2f
vlipe <felipevivenciorodrigues@gmail.com>
2024-08-19 00:17:03 -0300

    corrigir o carrossel.

--- VetMate/js/script-home.js
+++ VetMate/js/script-home.js
@@ -1,11 +1,13 @@
 const slides = document.querySelectorAll('.slide');
+const slideContainer = document.querySelector('.slides');
 const leftArrow = document.querySelector('.seta-esquerda');
 const rightArrow = document.querySelector('.seta-direita');
 let currentSlideIndex = 0;
 
 function showSlide(index) {
-    slides.forEach(slide => slide.classList.remove('active'));
-    slides[index].classList.add('active');
+    const slideWidth = slides[0].clientWidth;
+    const offset = -slideWidth * index;
+    slideContainer.style.transform = `translateX(${offset}px)`;
 }
 
 function nextSlide() {
