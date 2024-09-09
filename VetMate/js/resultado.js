baf34ebf84283db23f56680cfc62400e1f90a428
andreiolicar <andreiolicar7@gmail.com>
2024-09-09 11:10:16 -0300

    ajustes nos cards do buscador, criação da paginação para os resultados (não funcional e incompleto)

--- VetMate/js/resultado.js
+++
@@ -1,7 +0,0 @@
-const cardImg = document.querySelector(".card-container")
-
-window.onload = () => {
-    setTimeout(() => {
-        document.querySelector(".card-container").classList.remove("loading");
-    }, 2000);
-}
