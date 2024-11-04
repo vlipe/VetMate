eb43c900c1b1018d837a79fb5708fe682b6fc959
andreiolicar <andreiolicar7@gmail.com>
2024-11-04 20:44:42 -0300

    criação do chatbot completo e funcional.

--- VetMate/js/area-restrita-login.js
+++ VetMate/js/area-restrita-login.js
@@ -153,3 +153,215 @@ function esconderPrevBtn() {
     prevBtn.style.display = "flex"; 
   }
 }
+
+const btnChatbot = document.querySelector('.btn-chatbot');
+const containerChatbot = document.querySelector('.container-chatbot');
+const containerChatbotChat = document.querySelector('.container-chatbot-chat');
+const btnContinuar = document.querySelector('.btn-continuar');
+const svgFecharIcons = document.querySelectorAll('.svg-fechar');
+const svgFecharIcons2 = document.querySelectorAll('.svg-fechar-chat');
+const contentChatbot = document.querySelector('.content-chatbot');
+const inputMensagem = document.querySelector('.input-texto');
+const btnEnviar = document.querySelector('.btn-enviar');
+const setaVoltar = document.querySelector('.svg-voltar');
+
+let contentChatbotRemovido = false;
+
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+if (isMobile()) {
+  btnChatbot.style.position = 'fixed';
+  btnChatbot.style.bottom = '20px';
+  btnChatbot.style.right = '20px';
+} else {
+  btnChatbot.style.position = 'fixed';
+  btnChatbot.style.bottom = '3rem';
+  btnChatbot.style.right = '3rem';
+}
+
+containerChatbot.classList.remove('show');
+containerChatbotChat.classList.remove('show');
+
+btnChatbot.addEventListener('click', () => {
+  if (!containerChatbot.classList.contains('show')) {
+    containerChatbot.classList.add('show');
+    btnChatbot.style.opacity = '0';
+    btnChatbot.style.pointerEvents = 'none';
+  }
+});
+
+btnContinuar.addEventListener('click', () => {
+  containerChatbot.classList.remove('show');
+  containerChatbotChat.classList.add('show');
+});
+
+svgFecharIcons.forEach(icon => {
+  icon.addEventListener('click', () => {
+    if (containerChatbot.classList.contains('show')) {
+      containerChatbot.classList.remove('show');
+    } else if (containerChatbotChat.classList.contains('show')) {
+      containerChatbotChat.classList.remove('show');
+    }
+    setTimeout(() => {
+      btnChatbot.style.opacity = '1';
+      btnChatbot.style.pointerEvents = 'auto';
+    }, 300);
+  });
+});
+
+svgFecharIcons2.forEach(icon => {
+  icon.addEventListener('click', () => {
+    if (containerChatbot.classList.contains('show')) {
+      containerChatbot.classList.remove('show');
+    } else if (containerChatbotChat.classList.contains('show')) {
+      containerChatbotChat.classList.remove('show');
+    }
+    setTimeout(() => {
+      btnChatbot.style.opacity = '1';
+      btnChatbot.style.pointerEvents = 'auto';
+    }, 300);
+  });
+});
+
+setaVoltar.addEventListener('click', () => {
+  containerChatbotChat.classList.remove('show');
+  containerChatbot.classList.add('show');
+});
+
+function addMessage(text, isUser = false) {
+  const messagesContainer = document.querySelector('.messages-container');
+  if (messagesContainer) {
+    const message = document.createElement('div');
+    message.classList.add(isUser ? 'user-message' : 'bot-message');
+    message.innerText = text;
+    messagesContainer.appendChild(message);
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    return message;
+  }
+}
+
+async function sendMessageToAPI(userMessage) {
+  if (userMessage === "O que é a Vetmate?") {
+    return Promise.resolve("A Vetmate é um sistema único e inovador, cujo objetivo é revolucionar o dia a dia de pessoas como você! Auxiliando em tarefas como: organização de informações dos pets, encontrar clínicas veterinárias e muito mais!");
+  } else if (userMessage === "Onde posso encontrar uma clínica veterinária?") {
+    return Promise.resolve("Você pode encontrar uma clínica veterinária na seção 'Encontre um Veterinário' do nosso site.");
+  }
+
+  try {
+    const apiKey = 'AIzaSyDsiMgrvjrfgQlGlCJKm0K7FJx-UBFSV-w'; 
+    const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
+
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        contents: [{
+          parts: [{
+            text: userMessage
+          }]
+        }]
+      })
+    });
+
+    if (!response.ok) {
+      const statusCode = response.status;
+      throw new Error(`Erro na requisição à API. Status code: ${statusCode}`);
+    }
+
+    const data = await response.json();
+    const botResponse = data.candidates[0].content.parts[0].text;
+
+    return Promise.resolve(botResponse);
+
+  } catch (error) {
+    console.error(error);
+    return Promise.reject(`Erro: ${error.message}`);
+  }
+}
+
+btnEnviar.addEventListener('click', async () => {
+  const userMessage = inputMensagem.value.trim();
+  if (userMessage) {
+    iniciarChat(); 
+
+    addMessage(userMessage, true); 
+    inputMensagem.value = '';
+
+    const loadingMessage = addMessage("Aguarde...");
+
+    sendMessageToAPI(userMessage)
+      .then(botResponse => {
+        loadingMessage.remove();
+        addMessage(botResponse);
+      })
+      .catch(error => {
+        loadingMessage.remove();
+        addMessage(`Erro: ${error.message}`);
+      });
+  }
+});
+
+const sugestoesNavegacao = document.querySelectorAll('.section-chatbot:nth-child(1) .sugestion-chatbot');
+const sugestoesDuvidas = document.querySelectorAll('.section-chatbot:nth-child(2) .sugestion-chatbot');
+
+sugestoesNavegacao.forEach(sugestao => {
+  sugestao.addEventListener('click', () => {
+    const pergunta = sugestao.querySelector('.txt-sugestion-chatbot').textContent;
+
+    iniciarChat();
+    addMessage(pergunta, true);
+
+    if (pergunta === 'Aonde cadastro meus animais de estimação?') {
+      exibirResposta('Você pode cadastrar seus animais de estimação na seção "Meu Perfil" do nosso site.');
+    } else if (pergunta === 'Quero encontrar uma clínica veterinária!') {
+      exibirResposta('Para encontrar uma clínica veterinária, acesse a seção "Encontre um Veterinário" no nosso site.');
+    }
+  });
+});
+
+sugestoesDuvidas.forEach(sugestao => {
+  sugestao.addEventListener('click', () => {
+    const pergunta = sugestao.querySelector('.txt-sugestion-chatbot').textContent;
+
+    iniciarChat();
+    addMessage(pergunta, true);
+
+    enviarPergunta(pergunta);
+  });
+});
+
+function enviarPergunta(pergunta) {
+  const loadingMessage = addMessage("Aguarde...");
+
+  sendMessageToAPI(pergunta)
+    .then(botResponse => {
+      loadingMessage.remove();
+      addMessage(botResponse);
+    })
+    .catch(error => {
+      loadingMessage.remove();
+      addMessage(`Erro: ${error.message}`);
+    });
+}
+
+function exibirResposta(resposta) {
+  const mensagemAguarde = document.querySelector('.messages-container .message:last-child');
+  if (mensagemAguarde && mensagemAguarde.innerText === "Aguarde...") {
+    mensagemAguarde.remove();
+  }
+  addMessage(resposta);
+}
+
+function iniciarChat() {
+  const messagesContainer = document.querySelector('.messages-container');
+  if (!messagesContainer) {
+    const newMessagesContainer = document.createElement('div');
+    newMessagesContainer.classList.add('messages-container');
+    containerChatbotChat.insertBefore(newMessagesContainer, document.querySelector('.container-mensagem'));
+  }
+  contentChatbot.remove();
+}
